414dda13abaa873aee57e1ed4e272d234502ac18
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-13 10:35:07 -0500

    task1 Completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const suma = require('./utils/suma');
 const Car = require('./classes/car');
 
-console.log(suma(1, 2, 3));
+console.log("1+2+3=",suma(1, 2, 3));
 const car = new Car(2);
 car.showDoors();
 car.addDoors(2);
